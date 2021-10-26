c5a62e943286d244c23ffc5d31e91113fe33ad1e
bashiru98 <atulebashirubukari@gmail.com>
2021-10-27 00:25:11 +0300

    added database setup

---
+++ src/database/db.go
@@ -0,0 +1,24 @@
+package database
+
+import (
+	"complete_golang_backend/src/models"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	var err error
+
+	DB, err = gorm.Open(mysql.Open("root:root@tcp(db:3306)/ambassador"), &gorm.Config{})
+
+	if err != nil {
+		panic("Could not connect with the database!")
+	}
+}
+
+func AutoMigrate() {
+	DB.AutoMigrate(models.User{}, models.Product{}, models.Link{}, models.Order{}, models.OrderItem{})
+}
